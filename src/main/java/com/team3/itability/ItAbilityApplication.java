012a080016f0a54191e568dd8cc99dc990621f46
악덕 <rudejrqkr97@gmail.com>
2024-03-03 00:38:31 +0900

    :memo: [Modify] @EnableTransactionManagement 추가

--- src/main/java/com/team3/itability/ItAbilityApplication.java
+++ src/main/java/com/team3/itability/ItAbilityApplication.java
@@ -2,8 +2,10 @@ package com.team3.itability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ItAbilityApplication {
 
     public static void main(String[] args) {
