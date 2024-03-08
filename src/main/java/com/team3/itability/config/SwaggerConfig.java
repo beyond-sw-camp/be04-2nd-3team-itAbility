58fbc5e754a0735fad27bb27c93b394d8b4a5afd
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-03-08 16:38:37 +0900

    :memo: [Add] Swagger API 명세서

---
+++ src/main/java/com/team3/itability/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package com.team3.itability.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+/* 설명. http://localhost:8080/swagger-ui/index.html */
+@OpenAPIDefinition(
+        info = @Info(title = "itAbility API 명세서",
+        description = "itAbility 서비스 api 명세서",
+        version = "v1"))
+public class SwaggerConfig {
+
+}
