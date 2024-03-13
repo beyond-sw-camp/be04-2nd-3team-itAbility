9b7b3222902b855f77828944cda6624ad2c91e02
daersh <daersh@naver.com>
2024-03-13 16:06:47 +0900

    :memo: [Add] feign 추가

--- member-service/src/main/java/com/team3/memberservice/client/MemberServiceClient.java
+++ member-service/src/main/java/com/team3/memberservice/client/MemberServiceClient.java
@@ -1,5 +1,6 @@
 package com.team3.memberservice.client;
 
+import com.team3.memberservice.mypage.dto.RequestRecruitCategory;
 import com.team3.memberservice.mypage.entity.SkillEntity;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "swcamp-order-service", url = "localhost:8282") // gateway에 갈 클라이언트의 이름, gateway 주소
+@FeignClient(name = "ITABILITY-BOARD-SERVICE", url = "localhost:8000") // gateway에 갈 클라이언트의 이름, gateway 주소
 public interface MemberServiceClient{
 
     // gateway가 알고 있는 마이크로 서비스의 접두사(라우팅 시 설정한 요청 경로)를 추가하여 요청 경로를 작성한다.
-    @GetMapping("/board-service/users/{userId}")
-    List<SkillEntity> getUserOrders(@PathVariable String userId);
+    @GetMapping("/board-service/recruit/recruit-category-list")
+    List<RequestRecruitCategory> getRequest();
+
 }
---
+++ member-service/src/main/java/com/team3/memberservice/mypage/dto/RequestRecruitCategory.java
@@ -0,0 +1,13 @@
+package com.team3.memberservice.mypage.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class RequestRecruitCategory {
+    int recruitCategoryId;
+    String recruitName;
+}
