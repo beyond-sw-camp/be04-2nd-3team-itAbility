35c18f2f26b050f4dc030c9c40ed44999af685d9
daersh <daersh@naver.com>
2024-03-20 17:07:31 +0900

    :memo: [Modify] member controller 에러 수정

--- web-server/src/main/java/webserver/member/controller/memberController.java
+++ web-server/src/main/java/webserver/member/controller/memberController.java
@@ -6,18 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import webserver.member.service.MyPageService;
-import webserver.member.vo.Mypage;
 
 @Controller
 @RequestMapping("/mypage")
 public class memberController {
-    private final MyPageService myPageService;
-
-    public memberController(MyPageService myPageService) {
-        this.myPageService = myPageService;
-    }
-
 
     @GetMapping("/{memberId}")
     public String mypage(@PathVariable long memberId, ModelAndView mv){
