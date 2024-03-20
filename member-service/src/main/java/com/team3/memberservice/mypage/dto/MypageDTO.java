65685bd92788e74dbfc8317c1f1336689b0fc9ca
daersh <daersh@naver.com>
2024-03-20 18:23:30 +0900

    :memo: [Add] mypage 화면에서 restAPI 데이터 출력

--- member-service/src/main/java/com/team3/memberservice/mypage/dto/MypageDTO.java
+++ member-service/src/main/java/com/team3/memberservice/mypage/dto/MypageDTO.java
@@ -1,9 +1,11 @@
 package com.team3.memberservice.mypage.dto;
 
 import com.team3.memberservice.career.dto.CareerDTO;
+import com.team3.memberservice.career.dto.ResponseCareer;
 import com.team3.memberservice.skill.dto.ResponseSkill;
 import lombok.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -13,9 +15,43 @@ import java.util.List;
 @ToString
 public class MypageDTO {
 
-    private MemberProfileDTO memberProfile;
-    private List<CareerDTO> careerDTOList;
-    private List<ResponseSkill> memberSkill;
-    private List<ResponseRecruitCategory> responseRecruitCategory;
+//    private MemberProfileDTO memberProfile;
+//    private List<CareerDTO> careerDTOList;
+//    private List<ResponseSkill> memberSkill;
+//    private List<ResponseRecruitCategory> responseRecruitCategory;
+    /*mypage 필요한 정보
+    * memberInfo: 이름, 생일, 전화번호
+    * memberProfile: 닉네임
+    * career: 회사명, 입사일,퇴사일, 직급, 역할, 재직여부
+    * skill: 스킬이름만
+    * recruitCategory: 전문분야하나만
+    * */
 
+    private String name;
+    private String nickname;
+    private String phone;
+    private String birthDate;
+
+    private List<ResponseCareer> careers;
+
+    private List<String> skills;
+    private List<String> recruitCategories;
+
+
+    public MypageDTO(MemberProfileDTO profile, List<CareerDTO> careerDTOList, List<ResponseSkill> skillDTOS, List<ResponseRecruitCategory> recruitCategory) {
+        this.skills = new ArrayList<>();
+        this.recruitCategories = new ArrayList<>();
+        this.careers = new ArrayList<>();
+        this.name= profile.getMemberInfo().getName();
+        this.nickname = profile.getNickname();
+        this.phone = profile.getMemberInfo().getPhone();
+        this.birthDate = profile.getMemberInfo().getBirthDate();
+        careerDTOList.forEach(career
+                -> this.careers.add(new ResponseCareer(career.getCompanyName(), career.getStartDate()
+                                 , career.getEndDate(), career.getRole(), career.getAssignedTask()
+                                 , career.isCurrentJob())));
+
+        skillDTOS.forEach(skillDTO -> this.skills.add(skillDTO.getSkillName()));
+        recruitCategory.forEach(recCate-> this.recruitCategories.add(recCate.getRecruitName()));
+    }
 }
--- web-server/src/main/java/webserver/member/controller/memberController.java
+++ web-server/src/main/java/webserver/member/controller/memberController.java
@@ -2,6 +2,7 @@ package webserver.member.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class memberController {
 
     @GetMapping("/{memberId}")
-    public String mypage(@PathVariable long memberId, ModelAndView mv){
-//        Mypage mypage = myPageService.getMyPage(memberId);
-//        mv.addObject(mypage);
-        return "main";
+    public String mypage(){
+        return "mypage/mypage-2";
     }
 
 
