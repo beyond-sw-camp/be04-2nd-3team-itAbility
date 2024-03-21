b51d5c1a79d989b02de2bf0b6ce67c256686b0d3
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-03-21 17:07:04 +0900

    :memo: [Add] recruit 화면에서 restAPI 데이터 출력

---
+++ web-server/src/main/java/webserver/recruit/controller/recruitController.java
@@ -0,0 +1,13 @@
+package webserver.recruit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/recruit")
+public class recruitController {
+
+    @GetMapping("/{recruitId}")
+    public String recruit() {return "recruit/detail";}
+}
